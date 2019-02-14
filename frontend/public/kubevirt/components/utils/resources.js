f229da06eb851e77a2a778052e26a3e2ffb8cd68
Rastislav Wagner <rawagner@redhat.com>
2019-02-14 13:22:27 +0100

    RDP connection: Get nodePort if service is NodePort type
    https://bugzilla.redhat.com/show_bug.cgi?id=1677249

--- frontend/public/kubevirt/components/utils/resources.js
+++ frontend/public/kubevirt/components/utils/resources.js
@@ -140,7 +140,7 @@ const getRdpAddressPort = (rdpService, launcherPod) => {
     return null;
   }
 
-  const port = _.get(rdpPortObj, 'port');
+  let port = _.get(rdpPortObj, 'port');
   let address;
   switch (_.get(rdpService, 'spec.type')) {
     case 'LoadBalancer':
@@ -156,6 +156,7 @@ const getRdpAddressPort = (rdpService, launcherPod) => {
       }
       break;
     case 'NodePort':
+      port = _.get(rdpPortObj, 'nodePort');
       if (launcherPod) {
         address = _.get(launcherPod, 'status.hostIP');
       }
@@ -167,7 +168,7 @@ const getRdpAddressPort = (rdpService, launcherPod) => {
       console.error('Unrecognized Service type: ', rdpService);
   }
 
-  if (!address) {
+  if (!address || !port) {
     return null;
   }
 
