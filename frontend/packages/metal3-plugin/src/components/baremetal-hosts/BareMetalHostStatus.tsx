af773dc4f8d2823954345623ceac51f93dc6b96b
Rastislav Wagner <rawagner@redhat.com>
2020-06-25 15:46:52 +0200

    Add no power management status and alert for BMH

--- frontend/packages/metal3-plugin/src/components/baremetal-hosts/BareMetalHostStatus.tsx
+++ frontend/packages/metal3-plugin/src/components/baremetal-hosts/BareMetalHostStatus.tsx
@@ -1,45 +1,45 @@
 import * as React from 'react';
-import { Button } from 'patternfly-react';
-import { AddCircleOIcon } from '@patternfly/react-icons';
+import { Link } from 'react-router-dom';
+import { resourcePathFromModel } from '@console/internal/components/utils';
 import {
   ProgressStatus,
   SuccessStatus,
   ErrorStatus,
   Status,
-  StatusIconAndText,
-  getNamespace,
+  PopoverStatus,
+  InfoStatus,
 } from '@console/shared';
-import { RequireCreatePermission } from '@console/internal/components/utils';
+import { K8sResourceKind } from '@console/internal/module/k8s';
 import {
-  HOST_STATUS_DISCOVERED,
   HOST_PROGRESS_STATES,
   HOST_ERROR_STATES,
   HOST_SUCCESS_STATES,
   NODE_STATUS_UNDER_MAINTENANCE,
   NODE_STATUS_STARTING_MAINTENANCE,
   NODE_STATUS_STOPPING_MAINTENANCE,
+  HOST_STATUS_UNMANAGED,
+  HOST_INFO_STATES,
 } from '../../constants';
-import { BareMetalHostModel } from '../../models';
 import { getHostErrorMessage } from '../../selectors';
 import { StatusProps } from '../types';
 import MaintenancePopover from '../maintenance/MaintenancePopover';
 import { BareMetalHostKind } from '../../types';
-import { K8sResourceKind } from '@console/internal/module/k8s';
+import { BareMetalHostModel } from '../../models';
 
-// TODO(jtomasek): Update this with onClick handler once add discovered host functionality
-// is available
-export const AddDiscoveredHostButton: React.FC<{ host: BareMetalHostKind }> = (
-  { host }, // eslint-disable-line @typescript-eslint/no-unused-vars
-) => {
-  const namespace = getNamespace(host);
-
-  return (
-    <RequireCreatePermission model={BareMetalHostModel} namespace={namespace}>
-      <Button bsStyle="link">
-        <StatusIconAndText icon={<AddCircleOIcon />} title="Add host" />
-      </Button>
-    </RequireCreatePermission>
-  );
+export const HOST_STATUS_ACTIONS = {
+  [HOST_STATUS_UNMANAGED]: (host: BareMetalHostKind) => (
+    <p>
+      <Link
+        to={`${resourcePathFromModel(
+          BareMetalHostModel,
+          host.metadata.name,
+          host.metadata.namespace,
+        )}/edit`}
+      >
+        Add credentials
+      </Link>
+    </p>
+  ),
 };
 
 const BareMetalHostStatus: React.FC<BareMetalHostStatusProps> = ({
@@ -50,30 +50,67 @@ const BareMetalHostStatus: React.FC<BareMetalHostStatusProps> = ({
   nodeMaintenance,
 }) => {
   const statusTitle = title || status;
+  let hostStatus: JSX.Element;
+  const action = HOST_STATUS_ACTIONS[status]?.(host);
   switch (true) {
-    case status === HOST_STATUS_DISCOVERED:
-      return <AddDiscoveredHostButton host={host} />;
     case [NODE_STATUS_STARTING_MAINTENANCE, NODE_STATUS_UNDER_MAINTENANCE].includes(status):
-      return <MaintenancePopover title={statusTitle} nodeMaintenance={nodeMaintenance} />;
+      hostStatus = <MaintenancePopover title={statusTitle} nodeMaintenance={nodeMaintenance} />;
+      break;
     case [NODE_STATUS_STOPPING_MAINTENANCE, ...HOST_PROGRESS_STATES].includes(status):
-      return <ProgressStatus title={statusTitle}>{description}</ProgressStatus>;
+      hostStatus = (
+        <ProgressStatus title={statusTitle}>
+          {description}
+          {action}
+        </ProgressStatus>
+      );
+      break;
     case HOST_ERROR_STATES.includes(status):
-      return (
+      hostStatus = (
         <ErrorStatus title={statusTitle}>
           <p>{description}</p>
           <p>{getHostErrorMessage(host)}</p>
+          {action}
         </ErrorStatus>
       );
+      break;
     case HOST_SUCCESS_STATES.includes(status):
-      return <SuccessStatus title={statusTitle}>{description}</SuccessStatus>;
-    default:
-      return <Status status={status} title={statusTitle} />;
+      hostStatus = (
+        <SuccessStatus title={statusTitle}>
+          {description}
+          {action}
+        </SuccessStatus>
+      );
+      break;
+    case HOST_INFO_STATES.includes(status):
+      hostStatus = (
+        <InfoStatus title={statusTitle}>
+          {description}
+          {action}
+        </InfoStatus>
+      );
+      break;
+    default: {
+      const statusBody = <Status status={status} title={statusTitle} />;
+
+      hostStatus =
+        description || action ? (
+          <PopoverStatus title={statusTitle} statusBody={statusBody}>
+            {description}
+            {action}
+          </PopoverStatus>
+        ) : (
+          statusBody
+        );
+    }
   }
+
+  return hostStatus;
 };
 
 type BareMetalHostStatusProps = StatusProps & {
   host?: BareMetalHostKind;
   nodeMaintenance?: K8sResourceKind;
+  showCredentials?: boolean;
 };
 
 export default BareMetalHostStatus;
