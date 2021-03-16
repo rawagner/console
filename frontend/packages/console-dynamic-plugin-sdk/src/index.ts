d0f0f0425b2331318babe606fb90a7955d38b0cd
Vojtech Szocs <vszocs@redhat.com>
2021-03-16 17:46:00 +0100

    Simplify Console extension types

--- frontend/packages/console-dynamic-plugin-sdk/src/index.ts
+++ frontend/packages/console-dynamic-plugin-sdk/src/index.ts
@@ -1,2 +1,16 @@
-export * from './extensions';
-export * from './api';
+// Plugin API types
+export * from './api/useResolvedExtensions';
+
+// Extension types
+export * from './extensions/add-actions';
+export * from './extensions/catalog';
+export * from './extensions/cluster-settings';
+export * from './extensions/context-providers';
+export * from './extensions/feature-flags';
+export * from './extensions/file-upload';
+export * from './extensions/navigation';
+export * from './extensions/pages';
+export * from './extensions/pvc';
+export * from './extensions/redux';
+export * from './extensions/resource-metadata';
+export * from './extensions/yaml-templates';
